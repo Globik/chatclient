930c98e2846a82b8070f7f6d75c55354b70d34d4
Globik <globalikslivov@gmail.com>
2023-06-13 08:47:24 +0500

    https

--- dist/assets/VkView-be571221.js
+++
@@ -1,1 +0,0 @@
-import{g as a,o as e}from"./index-4b74bb05.js";const _={__name:"VkView",setup(n){const{getUserData:t}=a();return e(async()=>{await t()}),(s,o)=>null}};export{_ as default};
---
+++ dist/assets/VkView-ff82fda2.js
@@ -0,0 +1,1 @@
+import{g as a,o as e}from"./index-0ebac065.js";const _={__name:"VkView",setup(n){const{getUserData:t}=a();return e(async()=>{await t()}),(s,o)=>null}};export{_ as default};
