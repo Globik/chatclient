5e8a86362e40faec9871b193bf380e9bc56f99ec
Globik <globalikslivov@gmail.com>
2023-08-07 14:57:41 +0500

    a

--- dist/assets/RegisterView-dfe92c10.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as r,c}from"./index-645ffe06.js";const t={};function n(s,o){return r(),c("div",null," register page ")}const _=e(t,[["render",n]]);export{_ as default};
--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const auth = axios.create({
-  baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_HOST_DEV + "auth" : import.meta.env.VITE_API_HOST_PROD,
+  baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_HOST_DEV + "auth" : import.meta.env.VITE_API_HOST_PROD + "auth",
 });
 //http://api.chat-roulet.ru/api/auth"
 export const universal = axios.create({
